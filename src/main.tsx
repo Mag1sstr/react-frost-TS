d1a4958e9004146ba4cd27328cf3a11adea0007b
Mag1sstr <danilkarachev234@mail.ru>
2024-11-16 04:46:46 +0500

    Add ContextProvider and ReduxProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import AuthContextProvider from "./contexts/Auth/AuthContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <AuthContextProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthContextProvider>
   </StrictMode>
 );
