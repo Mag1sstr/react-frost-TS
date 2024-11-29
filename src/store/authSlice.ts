71f328c7720a83bddb77df6a7121dfe1cb4e3bbe
Mag1sstr <danilkarachev234@mail.ru>
2024-11-30 00:39:39 +0500

    add TS

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,70 +1,70 @@
-import { createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import { useSelector } from "react-redux";
-import { AppDispatch, RootState } from "./store";
-import { IUser } from "../components/BasketPage/BasketPage";
+import { createSlice } from "@reduxjs/toolkit"
+import axios from "axios"
+import { useSelector } from "react-redux"
+import { AppDispatch, RootState } from "./store"
+import { IUser } from "../components/BasketPage/BasketPage"
 
 interface authState {
-  user: null | IUser;
-  tokenInfo: string | null;
+   user: IUser | null
+   tokenInfo: string | null
 }
 
 const initialState: authState = {
-  user: null,
-  tokenInfo: localStorage.getItem("token"),
-};
+   user: null,
+   tokenInfo: localStorage.getItem("token"),
+}
 
 export const authSlice = createSlice({
-  name: "authSlice",
-  initialState,
-  reducers: {
-    getTokenInfo(state, action) {
-      state.tokenInfo = action.payload;
-    },
-    setUser(state, action) {
-      if (state.tokenInfo) {
-        state.user = action.payload;
-      }
-    },
-    deleteUser(state, action) {
-      state.user = action.payload;
-    },
-  },
-});
+   name: "authSlice",
+   initialState,
+   reducers: {
+      getTokenInfo(state, action) {
+         state.tokenInfo = action.payload
+      },
+      setUser(state, action) {
+         if (state.tokenInfo) {
+            state.user = action.payload
+         }
+      },
+      deleteUser(state, action) {
+         state.user = action.payload
+      },
+   },
+})
 
 export function signIn(username: string, password: string) {
-  return function (dispatch: AppDispatch) {
-    // dispatch(setTokenInfo())
+   return function (dispatch: AppDispatch) {
+      // dispatch(setTokenInfo())
 
-    axios
-      .post("https://frost.runtime.kz/api/auth/token", {
-        username: username,
-        password: password,
-      })
-      .then((resp) => {
-        dispatch(getTokenInfo(resp.data.access_token));
-        localStorage.setItem("token", resp.data.access_token);
-      })
-      .catch(() => {
-        throw new Error("token error");
-      });
-  };
+      axios
+         .post("https://frost.runtime.kz/api/auth/token", {
+            username: username,
+            password: password,
+         })
+         .then((resp) => {
+            dispatch(getTokenInfo(resp.data.access_token))
+            localStorage.setItem("token", resp.data.access_token)
+         })
+         .catch(() => {
+            throw new Error("token error")
+         })
+   }
 }
 export function getUser() {
-  return function (dispatch: AppDispatch) {
-    const token = useSelector((state: RootState) => state.auth.tokenInfo);
-    if (token) {
-      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-      axios
-        .post("https://frost.runtime.kz/api/auth/user", {}, {})
-        .then((resp) => {
-          dispatch(setUser(resp.data));
-        });
-    }
-  };
+   return function (dispatch: AppDispatch) {
+      const token = useSelector((state: RootState) => state.auth.tokenInfo)
+      if (token) {
+         axios.defaults.headers.common["Authorization"] = "Bearer " + token
+         axios
+            .post("https://frost.runtime.kz/api/auth/user", {}, {})
+            .then((resp) => {
+               dispatch(setUser(resp.data))
+            })
+      }
+   }
 }
 
 // Action creators are generated for each case reducer function
-export const { getTokenInfo, setUser, deleteUser } = authSlice.actions;
+export const { getTokenInfo, setUser, deleteUser } = authSlice.actions
 
-export default authSlice.reducer;
+export default authSlice.reducer
