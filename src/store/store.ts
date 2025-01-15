de27d112d0b757c3f14e8159c61e966326a46912
Daniil <danilkarachev234@mail.ru>
2025-01-16 03:16:22 +0500

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit"
-import authSlice from "./authSlice"
-import filterSlice from "./filterSlice"
-import newOrderSlice from "./newOrderSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import filterSlice from "./filterSlice";
+import newOrderSlice from "./newOrderSlice";
+import basketSlice from "./basketSlice";
 
 export const store = configureStore({
-   reducer: {
-      auth: authSlice,
-      filter: filterSlice,
-      newOrder: newOrderSlice,
-   },
-})
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+  reducer: {
+    auth: authSlice,
+    filter: filterSlice,
+    newOrder: newOrderSlice,
+    basket: basketSlice,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
