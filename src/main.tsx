3a56b6c04ba43768dfd60bfef1da9a01be1cc894
Daniil <danilkarachev234@mail.ru>
2025-01-16 03:16:32 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from "react"
+import { StrictMode, Suspense } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { Provider } from "react-redux"
 import { store } from "./store/store.ts"
 
+import "./i18n"
+
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
       <Provider store={store}>
-         <App />
+         <Suspense fallback={<div>Loading...</div>}>
+            <App />
+         </Suspense>
       </Provider>
    </StrictMode>
 )
